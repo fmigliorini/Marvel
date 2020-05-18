c3604bf68a834fe0c22e84b0d905a799d5e1f647
Facundo Migliorini <facundomigliorini@Facundos-MacBook-Pro.local>
2020-05-18 01:22:20 -0300

    Cleaning

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import './App.css';
-import { Router } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
-import Routes from './Routes';
+import React from "react";
+import { Router } from "react-router-dom";
+import { createBrowserHistory } from "history";
+import Routes from "./Routes";
 
 const history = createBrowserHistory();
 
